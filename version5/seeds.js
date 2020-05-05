2a022c4a9887fc8e292448a3a4429dc456daa273
Harshit <harshit19051915@gmail.com>
2020-05-05 21:43:34 +0000

    Add Styling to campgrounds/show.js

--- version5/seeds.js
+++ version5/seeds.js
@@ -5,17 +5,17 @@ var  mongoose = require('mongoose'),
 var data = [
       {name:"Water Bridge",
        image:"https://images.pexels.com/photos/132037/pexels-photo-132037.jpeg?cs=srgb&dl=seaport-during-daytime-132037.jpg&fm=jpg",
-       description:"Wooden Bridge over a lake"
+       description:"Wooden Bridge over a lake....Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
       },
       {
         name:"Mountain Pathway",
        image:"https://images.pexels.com/photos/2082949/pexels-photo-2082949.jpeg?cs=srgb&dl=high-angle-photography-of-mountain-pathway-2082949.jpg&fm=jpg",
-       description:"Pathway across green mountains"
+       description:"Pathway across green mountains..Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
       },
       {
         name:"Lonely Tree",
        image:"https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?cs=srgb&dl=flight-landscape-nature-sky-36717.jpg&fm=jpg",
-       description:"flight landscape nature sky"
+       description:"flight landscape nature sky...Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
       }
 ]
 
