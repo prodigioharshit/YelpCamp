f7f97d434cf1fd48fc9ca84ae7a3a4633551bc1e
Harshit <harshit19051915@gmail.com>
2020-07-31 14:21:27 +0000

    Add edit route, create edit.ejs and use method-override package

--- version10/app.js
+++ version10/app.js
@@ -1,13 +1,14 @@
-var express = require('express'),
-      app    = express(),
-    mongoose = require('mongoose'), 
-    bodyParser = require('body-parser'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local'),
-    User   = require('./models/user'),
-    Campground = require('./models/campgrounds.js'),
-    Comment = require('./models/comment.js'),
-        seedDB = require("./seeds");
+var express         = require('express'),
+    app             = express(),
+    mongoose        = require('mongoose'), 
+    bodyParser      = require('body-parser'),
+    passport        = require('passport'),
+    LocalStrategy   = require('passport-local'),
+    methodOverride  = require('method-override'),
+    User            = require('./models/user'),
+    Campground      = require('./models/campgrounds.js'),
+    Comment         = require('./models/comment.js'),
+    seedDB          = require("./seeds");
 
 var commentRoutes       =  require('./routes/comments.js'),
     campgroundRoutes    =  require('./routes/campgrounds.js'),
@@ -24,6 +25,7 @@ app.set("view engine","ejs");
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname + "/public")) //__dirname gives path of current working directory
+app.use(methodOverride("_method")); // method override
 
 //PASSPORT CONFIGURATION
     app.use(require('express-session')({
@@ -46,7 +48,6 @@ app.use(function(req,res,next){
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);// in router {mergeParams:true}
 app.use(authRoutes); //no common routes
-
 
 app.listen(3000,function(req,res){
     console.log("YelpCamp Server Started");
--- version10/routes/campgrounds.js
+++ version10/routes/campgrounds.js
@@ -65,6 +65,32 @@ router.get("/:id",function(req,res){
     //res.send("This will be show page one day");
 });
 
+
+// EDIT 
+router.get("/:id/edit",function(req,res){
+   Campground.findById(req.params.id,function(err,foundCampground){
+       if(err){
+           res.redirect("/campgrounds");
+       }
+       else{
+           res.render("campgrounds/edit",{campground:foundCampground});
+       }
+   });
+});
+
+// UPDATE
+router.put("/:id",function(req,res){
+   Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
+       if(err){
+           res.redirect("/campgrounds");
+       }
+       else{
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   }); 
+});
+
+
 //middleware isLoggedIn 
 function isLoggedIn(req , res , next){
     if(req.isAuthenticated() == true){
