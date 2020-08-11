3fadcb351b6b27b3ce8039498b3a243ff611b344
Harshit <harshit19051915@gmail.com>
2020-08-11 14:15:18 +0000

    Add delete route and delete button in show.ejs

--- version10/routes/campgrounds.js
+++ version10/routes/campgrounds.js
@@ -90,6 +90,18 @@ router.put("/:id",function(req,res){
    }); 
 });
 
+//DELETE (DESTROY)
+router.delete("/:id",function(req,res){
+    Campground.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.redirect("/campgrounds");
+        }
+        else{
+            res.redirect("/campgrounds")
+        }
+    });
+});
+
 
 //middleware isLoggedIn 
 function isLoggedIn(req , res , next){
